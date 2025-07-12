071290d1cde15be3606af9c20d6e52036ff30c4c
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-07-12 21:26:13 +0000

    9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 '''inserts a new document in a collection based on kwargs:
-
 Prototype: def insert_school(mongo_collection, **kwargs):
 mongo_collection will be the pymongo collection object
 Returns the new _id'''
+"""pymongo"""
+
+
 def insert_school(mongo_collection, **kwargs):
+    """function that inserts a new document in a collection
+    based on kwargs
+    Args:
+    mongo_collection (object): pymongo collection object
+    """
+
     pymongo =  mongo_collection.insertOne(**kwargs)
     return pymongo.insert_id
